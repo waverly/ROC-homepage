4bfd938d35d1d6f66b8d978a922eb622c1456eb6
Waverly Mandel <waverly@Waverlys-MacBook-Pro.local>
2018-08-12 23:03:36 -0700

    threejs integration

--- webpack.config.js
+++ webpack.config.js
@@ -3,9 +3,7 @@ var HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   entry: {
-    index: "./src/js/index.js",
-    portfolio: "./src/js/portfolio.js",
-    about: "./src/js/about.js"
+    index: "./src/js/index.js"
   },
   output: {
     filename: "[name].bundle.js",
@@ -20,18 +18,6 @@ module.exports = {
       template: "./src/index.html",
       chunks: ["index"],
       filename: "./index.html" //relative to root of the application
-    }),
-    new HtmlWebpackPlugin({
-      hash: true,
-      template: "./src/portfolio.html",
-      chunks: ["portfolio"],
-      filename: "./portfolio.html" //relative to root of the application
-    }),
-    new HtmlWebpackPlugin({
-      hash: true,
-      template: "./src/about.html",
-      chunks: ["about"],
-      filename: "./about.html" //relative to root of the application
     })
   ]
 };
